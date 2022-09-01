e7a2643ea6ce6fe47a63587953e68bf9ea4eba88
Thiago <thiago_boimer@hotmail.com>
2022-09-01 13:45:51 -0300

    added image to model

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -116,6 +116,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static') # os arquivos estáticos estão no projeto
 ]
 
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
