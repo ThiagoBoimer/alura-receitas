a4ae06f8a6352a7acb70a74ac985bd0071279065
Thiago <thiago_boimer@hotmail.com>
2022-09-01 17:57:21 -0300

    adding POST

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,15 @@
 from urllib import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        print('Usuário criado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
 
 def login(request):
     return render(request, 'usuarios/login.html')
