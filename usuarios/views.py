b202dbc79f576b51a11e4f6f330814a9f1679d71
Thiago <thiago_boimer@hotmail.com>
2022-09-02 10:58:46 -0300

    added csrf and sign up functionallity

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,31 @@
 from urllib import request
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
 
     if request.method == 'POST':
         nome = request.POST['nome']
-        print('Usuário criado com sucesso')
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('o campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('o campo email não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if (User.objects.filter(email=email).exists()):
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
