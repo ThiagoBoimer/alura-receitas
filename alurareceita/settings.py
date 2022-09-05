b7514f740f05fd61afbdd3d10ba5647fbc2b9d56
Thiago <thiago_boimer@hotmail.com>
2022-09-05 16:20:35 -0300

    little refactoring

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,12 @@ STATICFILES_DIRS = [
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
