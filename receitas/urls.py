fc67d7747dba24bebb38aad98d9fca151457d366
Thiago <thiago_boimer@hotmail.com>
2022-09-05 18:02:04 -0300

    modularização das views

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,13 +1,13 @@
 from unicodedata import name
 from django.urls import path
-from . import views # o . é para acessarmos o diretório que está o arquivo
+from .views import * # o . é para acessarmos o diretório que está o arquivo
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:receita_id>', views.receita, name='receita'),
-    path('buscar', views.buscar, name='buscar'),
-    path('cria/receita', views.cria_receita, name='cria_receita'),
-    path('deleta/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
-    path('edita/<int:receita_id>', views.edita_receita, name='edita_receita'),
-    path('atualiza_receita', views.atualiza_receita, name='atualiza_receita')
+    path('', index, name='index'),
+    path('<int:receita_id>', receita, name='receita'),
+    path('buscar', busca, name='buscar'),
+    path('cria/receita', cria_receita, name='cria_receita'),
+    path('deleta/<int:receita_id>', deleta_receita, name='deleta_receita'),
+    path('edita/<int:receita_id>', edita_receita, name='edita_receita'),
+    path('atualiza_receita', atualiza_receita, name='atualiza_receita')
 ]
